77a3e7b641e29f770aaa426911b6b886c5807193
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2020-09-29 22:21:33 +0300

    Add files via upload

---
+++ problema nr.11.py
@@ -0,0 +1,5 @@
+x=int(input("nr iepurilor la inceput de luna="))
+y=int(input("nr iepurilor morti="))
+z=int(input("nr iepurilor nascuti="))
+t=x-y+p
+print("la sfarsitul lunii in crestere sunt",t,"iepuri")
---
+++ problema nr.9.py
@@ -0,0 +1,6 @@
+a=int(input("introduceti a="))
+b=int(input("introduceti b="))
+c=int(input("introduceti c="))
+print(a,"+",b,"=",a+b)
+print(b,"+",c,"=",c+b)
+print(a,"+",c,"=",a+c)
