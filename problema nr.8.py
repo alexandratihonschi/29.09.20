b86a23899580730654b0df2215d88a919b191dc1
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2020-09-30 10:13:53 +0300

    Add files via upload

---
+++ problema nr.8.py
@@ -0,0 +1,4 @@
+a=int(input("introduceti 1 nr="))
+b=int(input("introduceti 2 nr="))
+c=int(input("introduceti 3 nr="))
+print(str(a)+str(b)+str(c),str(b)+str(c)+str(a),str(c)+str(b)+str(a),str(a)+str(c)+str(c),str(c)+str(a)+str(b))
